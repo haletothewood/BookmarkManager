1b887abe9514ec3cecc288f67a9cec4f5c3a1f5f
David Halewood <davidnhalewood@hotmail.com>
2017-10-19 12:58:37 +0100

    adding password confirmation

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -29,10 +29,12 @@ def create_multi_tagged_link
   click_button 'Create Link'
 end
 
-def sign_up
+def sign_up(email: 'david@example.com',
+  password: 'password',
+  password_confirmation: 'password')
   visit '/users/new'
-  expect(page.status_code).to eq(200)
-  fill_in :email,    with: 'david@example.com'
-  fill_in :password, with: 'password'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign Up'
 end
